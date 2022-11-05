4f88296bbf0c0efa7063ec68d5c607e1aa439989
Michael Halpin <halpinm2010@gmail.com>
2022-11-05 10:59:44 +0000

    fix upstream issue 5

--- src/bookmarks.js
+++ src/bookmarks.js
@@ -68,7 +68,7 @@ function updateBookmarksVar() {
  */
 function storeBookmarks() {
   if (bookmarks.length === 0) bookmarksBar.classList.add("hidden");
-  else bookmarksBar.classList.remove("hidden");
+  // else bookmarksBar.classList.remove("hidden");
 	window.localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
 	updateBookmarksVar();
 }
